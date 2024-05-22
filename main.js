96902e6ae8b35708a8af1f14b6d0030f1e657a04
pct <tienpc>
2024-05-22 12:39:26 +0700

    scroll listener for nav-bar

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-navBarItems = document.querySelectorAll(".nav-link")
+const navBarItems = document.querySelectorAll(".nav-link")
 navBarItems.forEach(item=>{
     item.addEventListener("click",(e)=>{
         navBarItems.forEach(element => {
@@ -6,4 +6,24 @@ navBarItems.forEach(item=>{
         });
         e.currentTarget.classList.add("nav-item-checked");
     })
+})
+
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll",()=>{
+    let current = ''
+    sections.forEach(section=>{
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.clientHeight
+        
+        if(window.scrollY>= (sectionTop - sectionHeight / 3)){
+            current = section.getAttribute('id');
+        }
+    })
+    navBarItems.forEach(item => {
+        item.classList.remove('nav-item-checked');
+        if (item.getAttribute('href').includes(current)) {
+            item.classList.add('nav-item-checked');
+        }
+    });
 })
